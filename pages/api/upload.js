b5e82d9d10c1bec316f937fadb384dd21e00641c
Rick Terrill <rick.terrill@axios.com>
2020-07-07 06:08:23 -0500

    rough upload file saver

--- pages/api/upload.js
+++ pages/api/upload.js
@@ -1,18 +1,73 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
 
+import Formidable from "formidable";
+const fs = require("fs");
+
 export const config = {
   api: {
-    bodyParser: {
-      // TODO: make size limit configurable in ENV (per 12f app)
-      // Should it be just "3" or "3mb"?
-      sizeLimit: "3mb",
-    },
+    bodyParser: false,
   },
 };
 
-export default (req, res) => {
-  console.log(req.body);
+export default uploadForm(handler);
 
-  res.statusCode = 200;
-  // res.json({ name: 'John Doe' })
-};
+function handler(req, res) {
+  try {
+    if (req.method === "POST") {
+      res.status(200).send(req.form);
+    } else {
+      throw String("Method not allowed");
+    }
+  } catch (error) {
+    res.status(400).json({ message: JSON.stringify(error, null, 2) });
+  }
+}
+
+function uploadForm(next) {
+  return function (req, res) {
+    return new Promise(async (resolve, reject) => {
+      try {
+        // configure Formidable instance
+        const form = new Formidable.IncomingForm({
+          multiples: true,
+          keepExtensions: true,
+        });
+        form.once("error", (err) => {
+          console.error(err);
+          reject(err);
+        });
+        form
+          .on("fileBegin", (name, file) => {
+            console.log("start uploading: ", file.name);
+          })
+          .on("aborted", () => console.log("Aborted..."));
+        form.once("end", () => {
+          console.log("Done!");
+        });
+
+        // Parse the request with Formidable
+        return form.parse(req, (err, fields, files) => {
+          if (err) {
+            reject(err);
+            // throw String(JSON.stringify(err, null, 2));
+          }
+          console.log(
+            "moving file: ",
+            files.file.path,
+            " to ",
+            `public/upload/${files.file.name}`
+          );
+          // TODO: support multiple files
+          fs.renameSync(files.file.path, `public/upload/${files.file.name}`);
+          req.form = { fields, files };
+
+          // pass along to next in chain
+          return resolve(next(req, res));
+        });
+      } catch (error) {
+        console.error(error);
+        return resolve(res.status(403).send(error));
+      }
+    });
+  };
+}
