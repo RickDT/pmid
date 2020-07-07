258d24896f50321565c9408ec7bfd33a218861a5
Rick Terrill <rick.terrill@axios.com>
2020-07-07 07:15:53 -0500

    investory API stub

---
+++ pages/api/investor.js
@@ -0,0 +1,8 @@
+// Next.js API route support: https://nextjs.org/docs/api-routes/introduction
+
+export default (req, res) => {
+  const body = JSON.parse(req.body);
+  console.log(body);
+
+  res.statusCode = 200;
+};
